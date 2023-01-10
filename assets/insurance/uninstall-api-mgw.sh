4286fd1e40e19c7d285c4c6bef339c1cf03f824c
Logeshwaran K P <kplogesh@gmail.com>
2023-01-10 11:09:23 +0530

    jaeger updates

--- assets/insurance/uninstall-api-mgw.sh
+++ assets/insurance/uninstall-api-mgw.sh
@@ -1,14 +1,6 @@
-echo "This script will begin the setup of insurance usecase using Software AG product stack and Observability components
+echo "This script will begin the uninstallation of insurance usecase
 
-Components will be created on the host - $HOSTNAME
-
-Pre-requisites should be configured as mentioned below
-
-Docker Credentials and Software AG's Container repo Credentials should be created as follows...
-
-kubectl create secret docker-registry regcred --docker-username=<username> --docker-password=<password> --docker-email=<email id>
-
-kubectl create secret docker-registry sagregcred --docker-server=sagcr.azurecr.io --docker-username=<username> --docker-password=<password> --docker-email=<email>
+Components will be deleted on the host - $HOSTNAME
 
 Please confirm to continue [Y|N]: "
 read choice
@@ -18,37 +10,20 @@ then
        echo "You have chosen to discontinue. No further actions."
        exit 1
 fi
-
-echo "Building Assessments Image"
 cd insurance-usecase-type-apigw-mgw/applications/
-docker build -f assessments/Dockerfile -t fitness-assessments:v1.0 .
 
-echo "Building Challenges Image"
-docker build -f challenges/Dockerfile -t fitness-challenges:v1.0 .
+echo "deleting local path provisioner"
 
-echo "Building Memberships Image"
-docker build -f memberships/Dockerfile -t fitness-memberships:v1.0 .
-
-echo "Building Points Image"
-docker build -f points/Dockerfile -t fitness-points:v1.0 .
-
-echo "Building Rewards Image"
-docker build -f rewards/Dockerfile -t fitness-rewards:v1.0 .
-
-echo "Installing local path provisioner for volumes"
-kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.23/deploy/local-path-storage.yaml
-
-echo "Installing Istio - Service Mesh"
+echo "Uninstall Istio - Service Mesh"
 cd ../../
 curl -L https://istio.io/downloadIstio | sh -
-export PATH="$PATH:$PWD/istio-1.16.1/bin"
-echo $PATH
-istioctl install --set profile=demo -y
-kubectl create ns insurance
-kubectl label namespace insurance istio-injection=enabled
+cd istio-1.16.1/bin
+./istioctl uninstall --purge
 
 echo "Install the applications - Insurance Usecase"
-kubectl apply -k k8s-env-manifests/overlays/app/
+kubectl delete -k k8s-env-manifests/overlays/app/
 
 echo "Install the Observability - Components"
-kubectl apply -k k8s-env-manifests/overlays/olly/
+kubectl delete -k k8s-env-manifests/overlays/olly/
+
+kubectl delete ns insurance monitoring
