cf9e9698bd1b60ded37a3f2e8fe42aea7fc5cfea
Logeshwaran Kannan Ponnurangam (LKA) <Logeshwaran.KannanPonnurangam@softwareag.com>
2023-02-13 16:56:28 +0530

    updates

--- assets/insurance/pipeline/stages/build/build.sh
+++ assets/insurance/pipeline/stages/build/build.sh
@@ -1,3 +1,3 @@
-cd assets/insurance/insurance-usecase-type-apigw-extes/applications/
+cd assets/insurance/insurance-usecase-type-apigw-extes/applications/tradingnetworks/
 ls -ltr
-docker build -t wm.msr.tn.app:r.$1 -f tradingnetworks/dev/Dockerfile .
+docker build -t wm.msr.tn.app:r.$1 -f dev/Dockerfile .
