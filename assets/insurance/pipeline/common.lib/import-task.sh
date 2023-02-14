5162e3baa5dd5f9aa509b4fafd64282dfad271cb
Logeshwaran Kannan Ponnurangam (LKA) <Logeshwaran.KannanPonnurangam@softwareag.com>
2023-02-14 15:37:56 +0530

    updates

--- assets/insurance/pipeline/common.lib/import-task.sh
+++ assets/insurance/pipeline/common.lib/import-task.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 kubectl config set-context --current --namespace=insurance
-kubectl create configmap tn-data-load --from-file=/ExportedData.bin
+cd /
+kubectl create configmap tn-data-load --from-file=ExportedData.bin
 cd assets/insurance/insurance-usecase-type-apigw-extes/applications/tradingnetworks/staging/manifests/job
 sed -i "s/<TAG>/$1/" asset-import-job.yaml
 kubectl apply -f ../tn-appconfig.yaml -f ../tn-env-script-cm.yaml -f .
