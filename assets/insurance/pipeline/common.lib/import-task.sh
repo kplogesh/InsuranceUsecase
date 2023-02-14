4db1b5fd94aad64d75da7275b04c6da02e613992
Logeshwaran Kannan Ponnurangam (LKA) <Logeshwaran.KannanPonnurangam@softwareag.com>
2023-02-14 20:14:11 +0530

    updates

--- assets/insurance/pipeline/common.lib/import-task.sh
+++ assets/insurance/pipeline/common.lib/import-task.sh
@@ -3,7 +3,7 @@
 kubectl config set-context --current --namespace=insurance
 sed -i "s/<TAG>/$1/" assets/insurance/insurance-usecase-type-apigw-extes/applications/tradingnetworks/dev/sourcecode/tn-assets/consolidated/TNExport.xml
 # Create the configmap with the exported data from source trading networks
-kubectl create configmap tn-data-load --from-file=ExportedData.zip --from-file=assets/insurance/insurance-usecase-type-apigw-extes/applications/tradingnetworks/dev/sourcecode/tn-assets/consolidated/TNExport.xml
+kubectl create configmap tn-data-load --from-file=ExportedData-$1.zip --from-file=assets/insurance/insurance-usecase-type-apigw-extes/applications/tradingnetworks/dev/sourcecode/tn-assets/consolidated/TNExport.xml
 
 # Modify the k8s job name with release iteration and apply the k8s job specifications 
 cd assets/insurance/insurance-usecase-type-apigw-extes/applications/tradingnetworks/staging/manifests/job
